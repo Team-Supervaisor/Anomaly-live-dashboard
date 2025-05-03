d37ba0d40b12a059f5fc7b03a47f1514703a0b21
Dilshad <dilshadahmad92425@gmail.com>
2025-05-03 14:24:00 +0530

    live-video page is created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Stores from './components/Home'
 import LoginModal from './components/LoginModal'
 import { useAppContext } from './context'
 import CameraRender from './components/CameraRender'
+import LiveVideo from './components/LiveVideo'
 function App() {
   const { isAuthenticated } = useAppContext();
 
@@ -16,6 +17,9 @@ function App() {
       {/* <Route path="/app" element={
        
       } /> */}
+      <Route path="/live-video" element={
+       <LiveVideo />
+      } />
   
    
     </Routes>
---
+++ src/components/LiveVideo.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import logo from '../assets/logo.png'
+import ai from '../assets/ai.png'
+import { RefreshCw } from 'lucide-react'
+import { Link } from 'react-router-dom'
+
+const LiveVideo = () => {
+    return (
+        <div className="flex flex-col bg-[#F5F9FF]">
+            <header className="flex justify-between items-center p-4 pb-2 ">
+                <Link to="/">
+                    <div className="flex items-center space-x-2 cursor-pointer">
+                        <div className="rounded">
+                            <img className="h-8 w-8" src={logo} alt="Logo" />
+                        </div>
+                        <h2 className="text-[22px] text-black font-medium">Anomaly Dashboard</h2>
+                    </div>
+                </Link>
+            </header>
+
+
+            <div className="flex flex-1 p-4 gap-4 overflow-hidden mt-2">
+                {/* Left Section */}
+                <div className="bg-white w-full rounded-[26px] p-4 min-h-[700px] flex flex-col">
+                    <div className='flex justify-between'>
+                        {/* hls videos */}
+                    </div>
+                </div>
+
+                {/* AI Analysis Card */}
+                <div className="bg-white rounded-[26px] p-4 w-[360px] max-h-[380px] flex flex-col">
+                    <div className="flex items-center justify-between mb-4">
+                        <div
+                            className="flex items-center cursor-pointer"
+                        // onClick={() => setShowAllAnamoly(true)}
+                        >
+                            <div className="w-6 h-6 rounded-full flex items-center justify-center mr-2">
+                                <img src={ai} alt="AI" className="w-4 h-4" />
+                            </div>
+                            <h2 className="font-medium text-lg">AI Analysis</h2>
+                            {/* {loader && (
+                  <Loader2 className="animate-spin text-indigo-500 w-4 h-4 ml-2" />
+                )} */}
+                        </div>
+                        <button
+                            // onClick={handleRefreshAi}
+                            className="p-2 bg-[#EBECFF] rounded hover:bg-[#DDE2FD] transition"
+                            title="Refresh AI analysis"
+                        >
+                            <RefreshCw className="w-5 h-5 text-[#5A62C8]" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default LiveVideo
