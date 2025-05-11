df7e3152ad24bb1afe4543ddd1ff736bf6900c95
umang-1305 <umangcharaya111@gmail.com>
2025-05-11 16:26:10 +0530

    Fix : Vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()
-  ],resolve: {
+    react(),
+  ],
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true, 
+    allowedHosts: ['.ngrok-free.app', 'sub.yourdomain.com', 'localhost', '127.0.0.1'], 
+  }
 })
