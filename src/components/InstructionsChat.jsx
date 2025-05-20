b2be781a0ccdcdb944d13052bb14c5ee5735b66a
Ravi <samcollins9899@gmail.com>
2025-05-21 00:22:32 +0530

    update: ui chages

--- src/components/InstructionsChat.jsx
+++ src/components/InstructionsChat.jsx
@@ -268,7 +268,7 @@ const InstructionsChat = ({
           />
           <button 
             onClick={toggleRecording}
-            className={`absolute right-2 top-[25px] -translate-y-1/2 w-8 h-8 rounded-full mb-2
+            className={`absolute right-2 2xl:right-6 top-[25px] -translate-y-1/2 w-8 h-8 rounded-full mb-2
               ${isRecording ? 'bg-red-100 text-red-500' : 'bg-[#717AEA1A]'}
               flex items-center justify-center transition-colors`}
           >
--- src/components/LIveAi.jsx
+++ src/components/LIveAi.jsx
@@ -783,7 +783,7 @@ const LiveAi = () => {
 
   return (
     <div className="flex flex-col h-screen bg-[#F5F9FF]">
-          <header className="flex items-center justify-between px-[41px] py-[10px] pl-[12px] bg-white">
+          <header className="flex items-center justify-between h-[58px] p-2 bg-white">
             {/* Left section - Logo and Title */}
             <Link to="/" className="flex-none">
               <div className="flex items-center space-x-2 cursor-pointer">
@@ -822,39 +822,39 @@ const LiveAi = () => {
           </div>
         </header>
 
-      <div className="flex flex-1 p-4 pt-0 gap-4 overflow-hidden mt-8">
+      <div className="flex flex-1 p-4 pt-0 gap-4 overflow-hidden mt-5">
         {/* Left Section */}
         <div className="w-full">
-  <div className="bg-white w-full rounded-[26px] overflow-hidden">
-    {streamUrl ? (
-      <div className="w-full h-full relative">
-        <img
-          ref={imgRef}
-          src={streamUrl}
-          className="w-full h-full rounded-xl"
-          alt="Live stream"
-        />
-        <div className="absolute top-2 left-2 bg-white text-black px-2 py-1 rounded text-sm font-medium">
-          Video Canvas 1
-        </div>
-      </div>
-    ) : (
-      <div className="w-full h-full min-h-[600px] flex items-center justify-center">
-        <div className="flex flex-col items-center">
-          <div className="w-[120px] h-[120px]">
-            <DotLottieReact
-              src="https://lottie.host/444798af-70b8-4920-a17d-c009411cfb64/a81fXDiwEN.lottie"
-              loop
-              autoplay
+      <div className="bg-white w-full rounded-[26px] overflow-hidden">
+        {streamUrl ? (
+          <div className="w-full h-full relative">
+            <img
+              ref={imgRef}
+              src={streamUrl}
+              className="w-full h-full rounded-xl"
+              alt="Live stream"
             />
+            <div className="absolute top-2 left-2 bg-white text-black px-2 py-1 rounded text-sm font-medium">
+              Video Canvas 1
+            </div>
           </div>
-          <span className="text-gray-600 text-lg font-medium mt-1">
-            AI analyzing video
-          </span>
-        </div>
+        ) : (
+          <div className="w-full h-full min-h-[600px] flex items-center justify-center">
+            <div className="flex flex-col items-center">
+              <div className="w-[120px] h-[120px]">
+                <DotLottieReact
+                  src="https://lottie.host/444798af-70b8-4920-a17d-c009411cfb64/a81fXDiwEN.lottie"
+                  loop
+                  autoplay
+                />
+              </div>
+              <span className="text-gray-600 text-lg font-medium mt-1">
+                AI analyzing video
+              </span>
+            </div>
+          </div>
+        )}
       </div>
-    )}
-  </div>
 
   {streamUrl && (
   <div className="mt-4 flex items-center justify-center backdrop-blur-md bg-white/70 h-[80px] rounded-[100px] w-full max-w-[500px] mx-auto gap-6 border border-white/40 shadow-lg">
