9a52864a9a381370c08edd09faba3967cb9bbbe6
Ravi <samcollins9899@gmail.com>
2025-05-20 23:07:10 +0530

    fix:  state clear on clicking redirection

--- src/components/CameraRender.jsx
+++ src/components/CameraRender.jsx
@@ -380,21 +380,48 @@ export default function CameraRender() {
     setVideoConfigData(null);
   };
 
+  const resetState = () => {
+    setOpen(true);
+    setCameraName("");
+    setRtspUrl("");
+    setCameras([]);
+    setSelectedTool("pointer");
+    setSelectedCamera(null);
+    setActiveTab("video");
+    setMaximizedCamera(null);
+    setUploadedVideos([]);
+    setUploadDialogOpen(false);
+    setSelectedVideo(null);
+    setMaximizedVideo(null);
+    setHasShapesSaved(false);
+    setHasVideoShapesSaved(false);
+    setVideoConfigData(null);
+    setCameraShapes({});
+    setVideoShapes({});
+    setGridKey(0);
+    setGridVideoKey(0);
+    setIsSaving(false);
+    setIsAdding(false);
+  };
+
   return (
     <div className="relative w-full h-full flex flex-col items-center">
       <div className="relative flex flex-col items-center w-screen h-screen">
         <header className="flex items-center justify-between h-[58px] pl-[12px] bg-white w-full">
           {/* Left section - Logo and Title */}
-          <Link to="/" className="flex-none">
-            <div className="flex items-center space-x-2 cursor-pointer">
-              <div className="rounded">
-                <img className="h-8 w-8" src={logo} alt="Logo" />
-              </div>
-              <h2 className="text-[22px] text-black font-medium">
-                Tracking Dashboard
-              </h2>
+          <div
+          onClick={resetState}
+          className="flex-none cursor-pointer"
+        >
+          <div className="flex items-center space-x-2">
+            <div className="rounded">
+              <img className="h-8 w-8" src={logo} alt="Logo" />
             </div>
-          </Link>
+            <h2 className="text-[22px] text-black font-medium">
+              Tracking Dashboard
+            </h2>
+          </div>
+        </div>
 
           {/* Right section - Fullscreen and Live AI */}
           <div className="flex items-center gap-4">
